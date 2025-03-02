562665344e6f764671a89c4e12b13810be4f86a9
soma <somahirao@gmail.com>
2025-03-02 10:18:04 +0900

    ♻️ 全体をコンポネント化

---
+++ components/Hero.js
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+export function Hero() {
+  return (
+    <>
+      <Image
+        className={styles.logo}
+        src="/next.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
+      <ol>
+        <li>
+          Get started by editing <code>pages/index.js</code>.
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+    </>
+  );
+}
